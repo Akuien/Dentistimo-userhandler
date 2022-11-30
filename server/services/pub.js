a640f728be9ee4828fd3bf00c73dc4c560ad536c
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2022-11-30 11:42:26 +0100

    added .env file and changed cluster

--- server/services/pub.js
+++ server/services/pub.js
@@ -1,14 +1,14 @@
 const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://127.0.0.1:1883') // public broker mqtt://broker.hivemq.com
+const client = mqtt.connect('mqtt://127.0.0.1:1883') 
 
 client.on('connect', function () {
-  client.subscribe('topic1/test', function () {
+  client.subscribe('user/auth', function () {
 
   })
   // Publish a new message every 4 seconds
   setInterval(function () {
     const rand = Math.random() * 100
-    client.publish('topic1/test', String(rand), function () {
+    client.publish('user/auth', String(rand), function () {
       console.log('Pushed: ' + rand)
     // client.end();
     })
--- server/services/sub.js
+++ server/services/sub.js
@@ -1,5 +1,6 @@
 const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://127.0.0.1:1883') // public broker mqtt://broker.hivemq.com
+const client = mqtt.connect('mqtt://127.0.0.1:1883') 
+
 
 client.on('connect', function () { // When connected
   // Subscribe to a topic
